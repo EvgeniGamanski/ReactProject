e1207d16a15d8b7574c6e6c7f1066213f62702b6
EvgeniGamanski <evgeny636@abv.bg>
2023-11-17 20:34:38 +0200

    Initial Commit

---
+++ project/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <div>
+    <h1>React Project Defence</h1>
+    <div>
+      <p>This project will soon be developed!</p>
+    </div>
+    </div>
+  )
+}
+
+export default App
